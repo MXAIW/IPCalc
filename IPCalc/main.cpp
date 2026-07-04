0de67a2b6c314e85fe62f239201b52b4b91bd789
MXAIW <marec13mam@gmail.com>
2026-07-04 18:53:47 +0700

    Mask & Prefix protected

--- IPCalc/main.cpp
+++ IPCalc/main.cpp
@@ -12,6 +12,14 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR ipCmdLine, IN
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	HWND hIPaddress = GetDlgItem(hwnd, IDC_IPADDRESS);
+	HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
+	HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
+	DWORD dwIPaddress = 0;
+	DWORD dwIPmask = 0;
+	DWORD dwIPprefix = 0;
+	CHAR szIPprefix[8] = {};
+	static BOOL bClassPrefix = TRUE;
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:
@@ -19,40 +27,71 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		break;
 	case WM_COMMAND:
 	{
+		/*
 		HWND hIPaddress = GetDlgItem(hwnd, IDC_IPADDRESS);
 		HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
 		HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
 		DWORD dwIPaddress = 0;
 		DWORD dwIPmask = 0;
 		DWORD dwIPprefix = 0;
-		CHAR szIPprefix[8] = {};
+		CHAR szIPprefix[8] = {};*/
 		switch (LOWORD(wParam))
 		{
 		case IDC_IPADDRESS:
 			SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
-			if (FIRST_IPADDRESS(dwIPaddress) < 128) SendMessage(hIPmask, IPM_SETADDRESS, 0, MAKEIPADDRESS(255, 0, 0, 0));
+			SendMessage(hEditPrefix, WM_GETTEXT, 32, (LPARAM)szIPprefix);
+			dwIPprefix = atoi(szIPprefix);
+			/*if (FIRST_IPADDRESS(dwIPaddress) < 128) SendMessage(hIPmask, IPM_SETADDRESS, 0, MAKEIPADDRESS(255, 0, 0, 0));
 			else if (FIRST_IPADDRESS(dwIPaddress) < 192) SendMessage(hIPmask, IPM_SETADDRESS, 0, MAKEIPADDRESS(255, 255, 0, 0));
-			else if (FIRST_IPADDRESS(dwIPaddress) < 224) SendMessage(hIPmask, IPM_SETADDRESS, 0, MAKEIPADDRESS(255, 255, 255, 0));
+			else if (FIRST_IPADDRESS(dwIPaddress) < 224) SendMessage(hIPmask, IPM_SETADDRESS, 0, MAKEIPADDRESS(255, 255, 255, 0));*/
+			if (dwIPprefix == 0 || dwIPprefix == 8 || dwIPprefix == 24)
+			{
+				if (FIRST_IPADDRESS(dwIPaddress) < 128) SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)"8");
+				else if (FIRST_IPADDRESS(dwIPaddress) < 192) SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)"16");
+				else if (FIRST_IPADDRESS(dwIPaddress) < 224) SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)"24");
+			}
 			break;
 		case IDC_IPMASK:
+			/*
 			SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
-			for (dwIPprefix = 0; dwIPmask; dwIPprefix++, dwIPmask <<= 1)
+			for (dwIPprefix = 0; dwIPmask; dwIPprefix++, dwIPmask <<= 1);//Тело этого цикла состоит из одного пустого выражения
 				sprintf(szIPprefix, "%i", dwIPprefix);
 			SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)szIPprefix);
+			*/
 			break;
 		case IDC_EDIT_PREFIX:
 		{
-			if (HIWORD(wParam) == EN_KILLFOCUS)
+			if (HIWORD(wParam) == EN_CHANGE)
 			{
 				SendMessage(hEditPrefix, WM_GETTEXT, 31, (LPARAM)szIPprefix);
-				INT iPrefix = atoi(szIPprefix); //atoi() - ANSI/ASCII string to INT. ANSI/ASCII-string (NULL-Terminal Line) - строка заканчивающаяся нулём. Массив элементов 'CHAR', последним элементом которого является '\0'
-				if (iPrefix > 32) SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)"32");
+				INT iPrefix = atoi(szIPprefix);	//atoi() - ANSI/ASCII string to INT. ANSI/ASCII-string (NULL-Terminal Line) - строка заканчивающаяся нулём. Массив элементов 'CHAR', последним элементом которого является '\0'
+				if (iPrefix > 32)
+				{
+					SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)"32");
+					iPrefix = 32;
+				}
+				dwIPmask = UINT_MAX;	//Все биты в Маске устанавливаются в 1
+				for (INT i = 0; i < 32 - iPrefix; dwIPmask <<= 1, i++);
+				SendMessage(hIPmask, IPM_SETADDRESS, 0, dwIPmask);
 			}
 		}
 		break;
 		case IDOK:
 			break;
 		case IDCANCEL:EndDialog(hwnd, 0);
+		}
+	}
+	break;
+	case WM_NOTIFY:
+	{
+		//if (wParam == IDC_IPMASK)
+		{
+			SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
+			for (dwIPprefix = 0; dwIPmask; dwIPprefix++, dwIPmask <<= 1);//Тело этого цикла состоит из одного пустого выражения
+			sprintf(szIPprefix, "%i", dwIPprefix);
+			//https://legacy.cplusplus.com/reference/cstdio/sprintf/?kw=sprintf
+			//https://legacy.cplusplus.com/reference/cstdio/printf/
+			SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)szIPprefix);
 		}
 	}
 	break;
